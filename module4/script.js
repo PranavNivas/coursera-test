6a2645c108c032cca8098a84331aeae3caada3bb
PranavNivas <144651776+PranavNivas@users.noreply.github.com>
2023-10-05 09:46:13 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,4 +1,3 @@
-
 // ******************************* 
 // START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
@@ -29,23 +28,21 @@ WARNING!!! WARNING!!!
 */
 
 
-// (function () {
-
-// var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-// })
 (function () {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
-    for (var i = 0; i < names.length; i++) {
-    
-      var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } 
-      else {
-        helloSpeaker.speak(names[i]);
-      }
-    }
-    
-    })();
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
+
+
